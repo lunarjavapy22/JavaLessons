7d398100adf07353286ab310f051e51053dcaed5
lunarjavapy22 <96844447+lunarjavapy22@users.noreply.github.com>
2022-03-01 22:14:40 +0300

    Update Metodlar.java

--- Lessons/Metodlar.java
+++ Lessons/Metodlar.java
@@ -1,3 +1,4 @@
+//Bu ilk kod.
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -13,5 +14,39 @@ public class MainActivity extends AppCompatActivity {
         //
         return a + b;
     }
+
+}
+//Bu 2. kod.
+
+package com.example.metodlar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        System.out.println("on resume");
+    }
+    //Farkı ise 2. kodda kendi oluşturduğum sınıfa özellikler atadım. Bu sınıfta ise kendi oluşturduğum sınıftan atadağım özelliklere göre değişkenler atadım.
+    public void whoscientist(){
+        Scientists newton = new Scientists();
+        newton.ArticleCount = 10;
+        newton.field = "Astropysics";
+        newton.name = "newton";
+        System.out.println(newton.ArticleCount);
+    }
+
 
 }
